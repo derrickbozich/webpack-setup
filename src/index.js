49a63d87dcc99264c3a90f7486bdfd8d3f350a22
Derrick <dbozich1@gmail.com>
2019-11-20 02:52:29 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import _ from "lodash";
+import './style.css';
+
+function component() {
+  const element = document.createElement("div");
+  const button = document.createElement("button");
+  const br = document.createElement("br");
+
+  button.innerHTML = "Click me and look at the console!";
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.appendChild(br);
+  element.appendChild(button);
+  element.classList.add('hello');
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e =>
+    import(/* webpackChunkName: "print" */ "./print").then(module => {
+      const print = module.default;
+
+      print();
+    });
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  // entry: {
+  //   index: "./src/index.js",
+  //   another: "./src/another-module.js"
+  // },
+  entry: [
+    // Add the client which connects to our middleware
+    // You can use full urls like 'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr'
+    // useful if you run your app from another point like django
+    "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000",
+    // And then the actual application
+    "./src/index.js"
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Output Management"
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+    // // OccurrenceOrderPlugin is needed for webpack 1.x only
+    // new webpack.optimize.OccurrenceOrderPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
+    // // Use NoErrorsPlugin for webpack 1.x
+    // new webpack.NoEmitOnErrorsPlugin()
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  }
+};
